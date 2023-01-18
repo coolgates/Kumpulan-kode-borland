633915fb45a55e9a1a01e89ce4168e805906fab5
coolgates <122433881+coolgates@users.noreply.github.com>
2023-01-18 09:31:52 +0700

    Add files via upload

---
+++ person.h
@@ -0,0 +1,14 @@
+program(char judd[49]="")
+{
+if(strcmp(judd,"PERSON")==0)
+{
+clrscr();
+cout<<"===================Biodata====================="<<endl;
+cout<<"Nama  : Fajar Zulfiansyah Haerudin"<<endl;
+cout<<"Kelas : X-PPLG"<<endl;
+cout<<"User  : COOLGATES"<<endl;
+cout<<"Pass  : texas"<<endl;
+cout<<"=============Masukan User dan Pass=============="<<endl;
+getch();
+}
+}
